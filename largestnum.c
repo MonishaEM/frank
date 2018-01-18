9acc55be9b7994714859b303536bbcd1806ae960
franklinpirate <35331683+franklinpirate@users.noreply.github.com>
2018-01-18 16:18:44 +0530

    Create largestnum.c

---
+++ largestnum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter three numbers");
+scanf("%d%d%d",&a,&b,&c);
+if(a>=b&&a>=c)
+printf("%d is large",a);
+if(b>=a&&b>=c)
+printf("%d is large",b);
+if else
+printf("%d is large",c);
+}
